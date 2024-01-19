58bf7e6ede94563726168a2df1d1e5b107c27c5e
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-01-19 18:14:22 -0500

    adding state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import CourseGoal from "./components/CourseGoal.tsx";
 import goalsImg from "./assets/goals.jpg";
 import Header from "./components/Header";
+import { useState } from "react";
+
+type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
 
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+  function handleAddGoal() {
+    setGoals();
+  }
   return (
     <main>
       <Header image={{ src: goalsImg, alt: "A list of goals" }}>
         <h1>Your Course Goals</h1>
       </Header>
+      <button onClick={handleAddGoal}>Add Goal</button>
       <CourseGoal title="Learn React + TS">
         <p>Learn from the groun up</p>
       </CourseGoal>
